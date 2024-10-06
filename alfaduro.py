c74cde2f8c3c921a1916704387ff1a1acafff054
cmbsby <data.cmbsby@gmail.com>
2024-10-07 05:08:21 +0700

    Update alfaduro.py

--- alfaduro.py
+++ alfaduro.py
@@ -14,7 +14,7 @@ db = mysql.connector.connect(
 )               
 
 dt = datetime.now(pytz.timezone('Asia/Jakarta'))
-timestamp = datetime.timestamp(dt,pytz.timezone('Asia/Jakarta')
+timestamp = datetime.timestamp(dt,pytz.timezone('Asia/Jakarta'))
 date_time = datetime.fromtimestamp(timestamp)
 ts = date_time.strftime("%d-%m-%Y %H:%M:%S")
 
